1216ce8f42957a4cbfcddda219109b1cc098eaec
obronchain <olivier.bronchain@uclouvain.be>
2021-11-15 14:40:03 +0100

    add arg parsing

--- main.py
+++ main.py
@@ -5,15 +5,31 @@ from leakage_oracle import LeakageOracle
 from it_sampling import it_sampling
 import glob
 import matplotlib.pyplot as plt
-import tikzplotlib
 
-d = 2
-b = 4
-f = 0.0
-sigma = 1
+## arguments
+import argparse
+
+parser = argparse.ArgumentParser(description="Comparing distinguishers convergence.")
+parser.add_argument("--shares", "-d", default=2, type=int, help="Number of shares.")
+parser.add_argument(
+    "--bits", "-b", default=4, type=int, help="Number of bits in target bits."
+)
+parser.add_argument("--std", default=1.0, type=float, help="Noise standard deviation.")
+parser.add_argument("--flaw", "-f", default=0.0, type=float, help="Flaw magnitude.")
+parser.add_argument(
+    "--repeat", "-r", default=2, type=int, help="Number of repeated experiments"
+)
+
+
+args = parser.parse_args()
+d = args.shares
+b = args.bits
+f = args.flaw
+sigma = args.std
+repeat = args.repeat
 
 rp = 0.01
-repeat  = 2
+n_profile = np.logspace(2.5, 6, 15, dtype=int)
 dir_name = "d%d_b%d_f%.3f_sigma%.3f" % (d, b, f, sigma)
 os.makedirs(dir_name, exist_ok=True)
 
@@ -21,19 +37,19 @@ methods = [
     {
         "label": "gt-esasca",
         "func": gt_sasca,
-        "n_profile": np.logspace(2.5, 6, 15, dtype=int),
+        "n_profile": n_profile,
         "repeat": repeat,
     },
     {
         "label": "rf",
         "func": rf,
-        "n_profile": np.logspace(2.5, 6, 15, dtype=int),
+        "n_profile": n_profile,
         "repeat": repeat,
     },
     {
         "label": "mlp",
         "func": mlp,
-        "n_profile": np.logspace(2.5, 6, 15, dtype=int),
+        "n_profile": n_profile,
         "repeat": repeat,
     },
 ]
@@ -54,7 +70,7 @@ def it_computation(methods):
         pi = np.zeros(len(n_profile))
         pi_std = np.zeros(len(n_profile))
 
-        print("----> Evaluate ",method["label"])
+        print("----> Evaluate ", method["label"])
         for _ in range(method["repeat"]):
             coin = np.random.randint(0, 2 ** 32)
             fname = os.path.join(dir_name, method["label"] + "_%d.npz" % (coin))
@@ -62,12 +78,15 @@ def it_computation(methods):
             for i, n in enumerate(n_profile):
                 pmf = method["func"](leakage[:n], shares[:n], d=d, b=b)
                 pi[i], pi_std[i] = it_sampling(pmf, loracle, rp=rp, floor=mi / 50)
-                print("--- "+f"{method['label']}: pi = {pi[i]:.4f} : n = {n:9d} : mi = {mi:.4f}")
+                print(
+                    "--- "
+                    + f"{method['label']}: pi = {pi[i]:.4f} : n = {n:9d} : mi = {mi:.4f}"
+                )
             np.savez(fname, pi=pi, pi_std=pi_std, n_profile=n_profile)
 
 
 def summarize(labels):
-    """ Summarize the output of it_computation to generate curves"""
+    """Summarize the output of it_computation to generate curves"""
 
     # get MI best estimation
     mi = []
@@ -97,7 +116,7 @@ def summarize(labels):
 
 
 def plot_summary(labels):
-    """ Plot the summarized curved """
+    """Plot the summarized curved"""
     summary = np.load(os.path.join(dir_name, "summary.npz"), allow_pickle=True)
     mi = summary["mi"]
     curves = summary["curves"]
@@ -110,7 +129,6 @@ def plot_summary(labels):
 
     plt.legend()
     plt.savefig(os.path.join(dir_name, "summary.pdf"))
-    tikzplotlib.save(os.path.join(dir_name, "summary.tex"))
     plt.show()
 
 
